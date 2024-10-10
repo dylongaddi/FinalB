2ba280abc9525a29beabc0e6c0e268bb2c3c06c5
dylongaddi <93099493+dylongaddi@users.noreply.github.com>
2024-10-09 17:11:09 -0700

    new parallax background, boss node player node

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -11,6 +11,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor() or Input.is_action_pressed("jump") and is_on_floor():
@@ -24,8 +26,8 @@ func _physics_process(delta):
 			sprite.flip_h = false
 		elif direction < 0:
 			sprite.flip_h = true
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()

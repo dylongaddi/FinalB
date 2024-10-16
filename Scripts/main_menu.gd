892f388a55dcf8fd673c810796cb3b63d6a30e69
dylongaddi <93099493+dylongaddi@users.noreply.github.com>
2024-10-16 15:37:53 -0700

    starting settings menu transition

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,13 +1,14 @@
 extends Control
 
-
+@onready var main = $Main
+@onready var settings = $Settings
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
@@ -17,8 +18,11 @@ func _on_play_pressed():
 
 
 func _on_settings_pressed():
+	main.hide()
+	settings.show()
 	pass # Replace with function body.
 
 
 func _on_quit_pressed():
+	get_tree().quit()
 	pass # Replace with function body.

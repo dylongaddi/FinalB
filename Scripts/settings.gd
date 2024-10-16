27b46f39b1da61dc3f5661fe03e4c7e5c13c0490
dylongaddi <93099493+dylongaddi@users.noreply.github.com>
2024-10-16 03:10:21 -0700

    organized files

---
+++ Scripts/settings.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_volume_value_changed(value):
+	AudioServer.set_bus_volume_db(0, value)
+	pass # Replace with function body.

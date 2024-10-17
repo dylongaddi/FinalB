94b0f4ff535c3b5091ea6fe66b27fe9a5dcba1e8
dylongaddi <93099493+dylongaddi@users.noreply.github.com>
2024-10-16 23:35:53 -0700

    adjust volume

--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -4,20 +4,19 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var settings = ConfigFileHandler.load_settings()
-	volumeSlider.value = min(settings.volume, 1.0) * 100
-	pass # Replace with function body.
-
+	if settings:
+		volumeSlider.value = min(settings.volume, 1.0) * 100
+	else: 
+		volumeSlider.value = -15
 
 func _on_volume_value_changed(value):
 	AudioServer.set_bus_volume_db(0, value)
-	pass # Replace with function body.
 
 
 
 func _on_quit_pressed():
 	get_tree().quit()
 	
-	pass # Replace with function body.
 
 
 func _on_volume_drag_ended(value_changed):

c4ed064e2eb377f8467252c131d5368250ba102a
dylongaddi <93099493+dylongaddi@users.noreply.github.com>
2024-10-18 02:45:11 -0700

    starting damage number implementation

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -5,6 +5,7 @@ class_name Bullet
 @export var velocity:Vector2
 @export var duration = 200
 @onready var sprite = $Sprite2D
+@onready var damageNumber = preload("res://Scenes/damage_number.tscn")
 
 var damage = 1
 var direction = Vector2.RIGHT
@@ -19,7 +20,17 @@ func _physics_process(delta):
 		var entity = collision.get_collider()
 		if entity is Boss:
 			entity.take_damage(damage)
+			var d = damageNumber.instantiate()
+			d.label_settings = LabelSettings.new()
+			if damage > 10:
+				d.label_settings.font_color = Color(255, 255, 0)
+			d.global_position = global_position
+			d.text = str(damage)
+			d.label_settings.outline_color = "#000"
+			d.label_settings.outline_size = 6
+			get_tree().current_scene.add_child(d)
 		queue_free()
+			
 		
 	duration -= 1
 	if duration < 0:

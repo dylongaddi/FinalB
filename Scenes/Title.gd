6b1c30692e45df15d35c0185fc2e34c13cf33e67
dylongaddi <93099493+dylongaddi@users.noreply.github.com>
2024-12-09 16:02:16 -0800

    fix text bug

--- Scenes/Title.gd
+++ Scenes/Title.gd
@@ -2,12 +2,9 @@ extends Label
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	await get_tree().create_timer(5).timeout
-	queue_free()
-	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	await get_tree().create_timer(5).timeout
+	queue_free()
 	pass
